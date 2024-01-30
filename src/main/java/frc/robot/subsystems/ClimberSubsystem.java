d2f5e398641e7a517833a9d9b16a54db5d4b97bb
scarlettWS <scarlettsaam@gmail.com>
2024-01-29 16:32:16 -0800

    Update ClimberSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -4,14 +4,43 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
+  CANSparkMax m_climberControllerRight = new CANSparkMax(0, MotorType.kBrushless);
+  CANSparkMax m_climberControllerLeft = new CANSparkMax(0, MotorType.kBrushless);
+  boolean firstRun = true;
+
+  RelativeEncoder m_climberEncoder;
+  SparkMaxPIDController m_climberPID;
+
   /** Creates a new ClimberSubsystem. */
-  public ClimberSubsystem() {}
+  public ClimberSubsystem() {
+    m_climberEncoder = m_climberControllerRight.getEncoder();
+    m_climberPID = m_climberControllerRight.getPIDController();
+
+  }
+
+  public void extend(){
+    m_climberControllerLeft.set(1); //speed will go here, 1 is a placecholder
+    m_climberControllerRight.follow(m_climberControllerLeft);//will make this one follow the left motor's actions, might need inversions
+  }
+
+  public void yoink(){
+    m_climberControllerLeft.set(-1); //speed will go here, -1 is a placecholder
+    m_climberControllerRight.follow(m_climberControllerLeft);
+  }
 
   @Override
   public void periodic() {
+    if(firstRun){
+      yoink();
+    }
     // This method will be called once per scheduler run
   }
 }
