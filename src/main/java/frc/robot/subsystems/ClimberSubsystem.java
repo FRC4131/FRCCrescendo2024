7112bc8c56adde98f1a3b9c09d73bc5b31aa6331
Scarlett using august's laptop <scarlettsaam@gmail.com>
2024-02-06 17:00:38 -0800

    update climber
    see title

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkLowLevel.MotorType;
@@ -12,28 +13,30 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
-  CANSparkMax m_climberControllerRight = new CANSparkMax(0, MotorType.kBrushless);
-  CANSparkMax m_climberControllerLeft = new CANSparkMax(0, MotorType.kBrushless);
+  CANSparkMax m_climberController = new CANSparkMax(0, MotorType.kBrushless);//get ID later
+  private RelativeEncoder m_climberEncoder;
+  private SparkMaxPIDController m_climberPID;
   boolean firstRun = true;
-
-  RelativeEncoder m_climberEncoder;
-  SparkMaxPIDController m_climberPID;
 
   /** Creates a new ClimberSubsystem. */
   public ClimberSubsystem() {
-    m_climberEncoder = m_climberControllerRight.getEncoder();
-    m_climberPID = m_climberControllerRight.getPIDController();
-
+    m_climberEncoder = m_climberController.getEncoder();
+    m_climberPID = m_climberController.getPIDController();
   }
 
+  public void setClimberSpeed(double speed){
+    m_climberPID.setReference(speed, ControlType.kDutyCycle); //unsure about control type
+  }
   public void extend(){
-    m_climberControllerLeft.set(1); //speed will go here, 1 is a placecholder
-    m_climberControllerRight.follow(m_climberControllerLeft);//will make this one follow the left motor's actions, might need inversions
+    //the climber gets released
   }
 
   public void yoink(){
-    m_climberControllerLeft.set(-1); //speed will go here, -1 is a placecholder
-    m_climberControllerRight.follow(m_climberControllerLeft);
+    while(//motor is not all the way pulled in);
+    {
+      //pull in motor
+    }
+    //have the motor pull in the climber, use limits
   }
 
   @Override
