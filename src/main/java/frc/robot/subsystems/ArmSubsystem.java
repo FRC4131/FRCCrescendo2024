e261c40e7abe4a2b0b3c2588411b9d0c0213daca
Akash Krishna <104955274+akrishnob@users.noreply.github.com>
2024-02-24 23:55:53 -0800

    comments.java

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -22,22 +22,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.ArmConstants;
 
+// arm subsystem controls the pivot of the arm/indexing mechanism. uses 2 motors. in this iteration: left = leader, right = follower
+
 public class ArmSubsystem extends SubsystemBase {
-  /** Creates a new SwivelArmSubsystem. */
   private CANSparkMax m_armMotorL;  //lead/left motor
   private CANSparkMax m_armMotorR; //follower/right motor
   private RelativeEncoder m_armEncoder; 
   private PIDController m_armPidController; 
-  private ArmFeedforward m_armFeedforward; 
+  //private ArmFeedforward m_armFeedforward; 
   private double m_angleSetpoint = 23.0;
   private boolean m_isManualMode; 
 
   public ArmSubsystem() {
-    m_armMotorL = new CANSparkMax(Constants.ArmConstants.ARM_MOTOR_ID_LEFT, MotorType.kBrushless);
+
+    //configures left motor (inverted) 
+    m_armMotorL = new CANSparkMax(Constants.ArmConstants.ARM_MOTOR_ID_LEFT, MotorType.kBrushless); 
     m_armMotorL.setSmartCurrentLimit(30); 
     m_armMotorL.setIdleMode(IdleMode.kCoast);
     m_armMotorL.setInverted(true);
 
+    //configures right motor (not inverted) 
     m_armMotorR = new CANSparkMax(Constants.ArmConstants.ARM_MOTOR_ID_RIGHT, MotorType.kBrushless);
     m_armMotorR.follow(m_armMotorL, true);
     m_armMotorR.setSmartCurrentLimit(30); 
@@ -46,7 +50,7 @@ public class ArmSubsystem extends SubsystemBase {
     m_armEncoder = m_armMotorL.getEncoder(); 
 
     m_armPidController = new PIDController(0.02, 0, 0);
-    m_armFeedforward = new ArmFeedforward(0, 0, 0, 0); 
+    //m_armFeedforward = new ArmFeedforward(0, 0, 0, 0); 
 
     m_armEncoder.setPositionConversionFactor(Constants.ArmConstants.ARM_ENCODER_SCALING_FACTOR);
     m_armEncoder.setPosition(Constants.ArmConstants.ARM_RESTING_POSITION_ANGLE);
@@ -64,7 +68,7 @@ public class ArmSubsystem extends SubsystemBase {
     }, this);
   }
 
-  public Command manualModeCommand(double power)
+  public Command manualModeCommand(double power) //puts arm into manual mode 
   {
     return new InstantCommand(() -> {
       m_isManualMode = true; 
@@ -72,29 +76,23 @@ public class ArmSubsystem extends SubsystemBase {
     }, this);
   }
 
-  public Command manualModeOffCommand()
+  public Command manualModeOffCommand() //takes arm out of manual mode and sets pid to its last angle
   {
     return new InstantCommand(() -> {
+      m_angleSetpoint = this.getArmAngle();
       m_isManualMode = false; 
     }); 
   }
 
-  public Command rotateToAngleCommand(double angle) {
+  public Command rotateToAngleCommand(double angle) { 
     return runOnce(() -> {
       goToAngle(angle);
     });
   }
 
-  private void goToAngle(double angleDegrees)
+  private void goToAngle(double angleDegrees) //sets arm to a specified angle 
   {
     m_angleSetpoint = angleDegrees; 
-  }
-
-  public Command goToAmpAngleCommand()
-  {
-    return runOnce(() -> {
-      goToAngle(ArmConstants.ARM_AMP_ANGLE);
-    }); 
   }
 
   public Command resetArmEncoderCommand()
@@ -104,20 +102,13 @@ public class ArmSubsystem extends SubsystemBase {
       m_armEncoder.setPosition(0.0);
     }); 
   }
-
-  public Command goToRestAngleCommand()
-  {
-    return runOnce(() -> {
-      goToAngle(ArmConstants.ARM_RESTING_POSITION_ANGLE);
-    }); 
+  
+  public void resetPosition() { 
+    this.goToAngle(Constants.ArmConstants.ARM_RESTING_POSITION_ANGLE); 
+    m_armEncoder.setPosition(23);
   }
 
-  public void resetPosition() { //sends arm to 0 position and resets encoder to 0 
-    this.goToRestAngleCommand(); 
-    m_armEncoder.setPosition(0);
-  }
-
-  public double getArmAngle()
+  public double getArmAngle() //returns current angle of the arm 
   {
     return m_armEncoder.getPosition(); 
   }
@@ -127,7 +118,7 @@ public class ArmSubsystem extends SubsystemBase {
     if (!m_isManualMode)
     {
         double rawPower = m_armPidController.calculate(getArmAngle(), m_angleSetpoint);
-        double clampedPower = MathUtil.clamp(rawPower, -0.08, 0.08);
+        double clampedPower = MathUtil.clamp(rawPower, -0.08, 0.08); //clamp power to 8% 
         m_armMotorL.set(clampedPower);
         SmartDashboard.putNumber("RawPower", rawPower);
         SmartDashboard.putNumber("ClampedPower", clampedPower);
