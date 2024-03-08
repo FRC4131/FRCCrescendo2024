f0e35757433abafc47e94a4b891b50f664c64013
2manydaves3003 <91045919+2manydaves3003@users.noreply.github.com>
2024-03-07 22:33:36 -0800

    Removing manualModeOff reference
    reference was causing it to not compile

--- src/main/java/frc/robot/commands/ArmJoystickCommand.java
+++ src/main/java/frc/robot/commands/ArmJoystickCommand.java
@@ -35,7 +35,7 @@ public class ArmJoystickCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_ArmSubsystem.manualModeOff(); 
+    //m_ArmSubsystem.manualModeOff(); 
   }
 
   // Returns true when the command should end.
